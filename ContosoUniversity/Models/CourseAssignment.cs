32af87c1969ad360b5ca4f659effcd31ab9e324c
Naoya <kusuriuri12121@yahoo.co.jp>
2020-08-10 16:45:30 +0900

    関連データの読み取り

--- ContosoUniversity/Models/CourseAssignment.cs
+++ ContosoUniversity/Models/CourseAssignment.cs
@@ -2,27 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
-    public enum Grade
+    public class CourseAssignment
     {
-        A,B,C,D,F
-    }
-
-    public class Enrollment
-    {
-        public int EnrollmentID { get; set; }
+        public int InstructorID { get; set; }
         public int CourseID { get; set; }
-        public int StudentID { get; set; }
-        [DisplayFormat(NullDisplayText ="No grade")]
-        public Grade? Grade { get; set; }
-
+        public Instructor instructor { get; set; }
         public Course Course { get; set; }
-        public Student Student { get; set; }
 
     }
 }
